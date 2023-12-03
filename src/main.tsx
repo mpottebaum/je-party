77413a9bfd3fe7dd8a1dbd7fb5e0ac2d8039b6be
Mike Pottebaum <michael.pottebaum@cdw.com>
2023-12-02 18:47:03 -0600

    work: add msw to intercept api requests in dev

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { SetupWorker } from 'msw/browser'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+  // @ts-expect-error TODO: will fix
+  const { worker } = (await import('./mocks/browser.js')) as { worker: SetupWorker;}
+  return worker.start()
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
