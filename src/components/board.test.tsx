7b5127dda5d8d7123db0a6bdc0ec1d2d9571be63
Mike Pottebaum <michael.pottebaum@cdw.com>
2023-12-04 21:04:04 -0600

    build better board tests

--- src/components/board.test.tsx
+++ src/components/board.test.tsx
@@ -1,5 +1,5 @@
 import { expect, it, describe, vi, afterEach } from 'vitest'
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Board } from './board'
 import { mockCategories } from '../mock-data'
 
@@ -18,5 +18,84 @@ describe('Board', () => {
       />,
     )
     expect(baseElement).toBeTruthy()
+  })
+
+  it('should give control of clue click behavior thru props', async () => {
+    const mockCategory = mockCategories[0]
+    const mockClue = mockCategory.clues[100]
+    const clueClickSpy = vi.fn()
+    render(
+      <Board
+        categories={mockCategories}
+        isCategoriesLoading={false}
+        answeringQuestion={true}
+        currentClue={mockClue}
+        handleClueClick={clueClickSpy}
+      />,
+    )
+
+    fireEvent.click(screen.getAllByText('$100')[0])
+    expect(clueClickSpy).toHaveBeenCalledTimes(1)
+    expect(clueClickSpy).toHaveBeenCalledWith(mockClue)
+  })
+
+  it('should highlight selected clue', async () => {
+    const mockCategory = mockCategories[0]
+    const mockClue = mockCategory.clues[100]
+    render(
+      <Board
+        categories={mockCategories}
+        isCategoriesLoading={false}
+        answeringQuestion={true}
+        currentClue={mockClue}
+        handleClueClick={vi.fn()}
+      />,
+    )
+
+    const [clueText] = screen.getAllByText(`$${mockClue.value}`)
+    const clueContainer = clueText.parentElement
+    expect(clueContainer?.className).toContain('selected')
+  })
+
+  it('should hide answered clues', async () => {
+    const mockCategory = mockCategories[0]
+    mockCategory.clues[100].answered = true
+    render(
+      <Board
+        categories={mockCategories}
+        isCategoriesLoading={false}
+        answeringQuestion={false}
+        handleClueClick={vi.fn()}
+      />,
+    )
+
+    const categoryTitle = screen.getByText(mockCategory.title)
+    const clueContainer = categoryTitle.parentElement?.nextElementSibling
+    const valueText = clueContainer?.children[0]
+    expect(clueContainer?.className).toContain('answered')
+    // value text is hidden
+    expect(valueText?.children).toHaveLength(0)
+  })
+
+  it('should hide clues with invalid values', async () => {
+    const mockCategory = mockCategories[0]
+    const mockClue = {
+      ...mockCategory.clues[100],
+      id: 999999999999999,
+      value: 600,
+    }
+    mockCategory.clues[mockClue.value] = mockClue
+
+    render(
+      <Board
+        categories={mockCategories}
+        isCategoriesLoading={false}
+        answeringQuestion={false}
+        handleClueClick={vi.fn()}
+      />,
+    )
+
+    const clueText = screen.queryByText(`$${mockClue.value}`)
+    expect(clueText).toBeFalsy()
   })
 })
